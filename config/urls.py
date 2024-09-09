4c047730e3d4bec2492ae9520ece4d0189f7b16f
alimehdi <alimehdi9651@gmail.com>
2024-09-10 02:24:07 +0530

    project setup

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('admin/', admin.site.urls),
+    path('', include('app.urls')),
 ]
 
 if settings.DEBUG:
